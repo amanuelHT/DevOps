ecc770a8c646676543bfc9aecf503dcb0df0006d
Amanuel Hayele Tsegay <amanuelht@ikt206-g-25v-amanuelht.novalocal>
2025-05-15 16:05:55 +0000

    uupp

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database_file/flask.db")
+
+
 SECRET_KEY = "fdsafasd"
 UPLOAD_FOLDER = "image_pool"
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
