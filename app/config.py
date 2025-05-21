b406f1d78e8706bf120ac123df47aa1b1f0eaf42
Amanuel Hayele Tsegay <amanuelht@ikt206-g-25v-amanuelht.novalocal>
2025-05-21 12:13:49 +0000

    update

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
-SECRET_KEY = "fdsafasd"
-UPLOAD_FOLDER = "image_pool"
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+import os
+
+class BaseConfig:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "defaultsecret")
+    UPLOAD_FOLDER = "image_pool"
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+def get_config():
+    return BaseConfig
